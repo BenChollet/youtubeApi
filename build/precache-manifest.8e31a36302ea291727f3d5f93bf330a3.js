520cb632571f933a2c1077553186fbef6596bc82
Benjamin Chollet <benjamin.chollet.pro@gmail.com>
2019-08-13 23:25:19 +0200

    First Commit

---
+++ build/precache-manifest.8e31a36302ea291727f3d5f93bf330a3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0323e4450d9067ec90bfb259976bebb3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbfdd6c2c176e02df4fb",
+    "url": "/static/js/2.914ff7cd.chunk.js"
+  },
+  {
+    "revision": "2ef3f5d8f1ba76707611",
+    "url": "/static/js/main.28910283.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
